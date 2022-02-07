4eeb11870bb404f8fe5ad68cf13ca1eda96c9e8e
evan <evansullivan74@gmail.com>
2022-02-07 18:47:20 +0000

    finished method rangeof ,using a switch statement, prints out where the value lies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,7 +13,7 @@
 
 fun main(args: Array<String>) {
     minimumOf(5,7)
-    rangeOf(4)
+    rangeOf(23)
     inferredTypeOf(0.0F)
     stringContains("wordle")
 }
@@ -28,10 +28,14 @@ fun minimumOf(number1: Long, number2: Long){
 }
 
 fun rangeOf(number: Int){
-    //TODO This function takes in a number and prints out the range that the number falls in.
-    // The ranges are: (0 or below) (1 to 5), (6 to 10), (11 to 20), (21+)
-    // For example, if the number was 7, print out a message saying that the number 7 falls into the range 6 to 10.
-}
+    when{
+        number <= 0-> println("the number $number is either less than or equal to zero");
+       number in 1..5 -> println("the number $number falls in the range of 1 to 5");
+       number in 6..10 -> println("the number $number falls in the range of 6 to 10");
+       number in 11..20 -> println("the number $number falls in the range of 11 to 20");
+        number >= 21 -> println("the number $number is either greater than or equal to 21");
+    }
+   }
 
 fun inferredTypeOf(variable: Any){
     //TODO This function examines the inferred type of the variable passed as a parameter.
